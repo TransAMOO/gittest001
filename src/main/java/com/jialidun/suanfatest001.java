52abb174b4ceaafd07d9500d8c686389847f1b1e
name <1813187257@qq.com>
2022-08-14 22:48:39 +0800

    第一次提交

---
+++ src/main/java/com/jialidun/suanfatest001.java
@@ -0,0 +1,13 @@
+package com.jialidun;
+
+public class suanfatest001 {
+    public static void main(String[] args) {
+        fun1();
+    }
+
+    private static String fun1() {
+
+        System.out.println("初始化");
+        return null;
+    }
+}
